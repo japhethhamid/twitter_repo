902a6cf7bb264270463499a2ee0e63808d7f54c3
jeff <japhethhamid@gmail.com>
2023-03-04 12:02:08 +0000

    class31 organization

---
+++ devops.java
@@ -0,0 +1,11 @@
+#!/bin/bash
+echo "please enter organization name"
+read name
+if (( $name==ziontech ))
+then 
+echo " welcome to ziontech "
+echo " thank you for coming "
+else
+echo " please try again "
+echo " go to the next building "
+fi 
