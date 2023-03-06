995c3b748d0e92bc146e5b74c3cb93fa6a00ec38
jeff <japhethhamid@gmail.com>
2023-03-06 05:19:07 +0000

    please resolve error code

---
+++ class31devops.java
@@ -0,0 +1,12 @@
+#!/bin/bash
+echo " please enter the name of your group "
+read name
+if (($name==group36))
+then 
+echo " wellcome to group36 "
+echo " the engineering group "
+else 
+echo " sorry you are in wrong group "
+echo " go to the next group "
+fi
+
